24ffa2ca3690cd4685d6cf494a9a56155cfa970d
bhavit-jfb <jfbrecov@gmail.com>
2026-03-02 00:41:40 +0530

    Update index.py

--- chat-app/api/index.py
+++ chat-app/api/index.py
@@ -1,13 +1,13 @@
 import os
 from flask import Flask
-from extensions import db
+# from extensions import db
 
 app = Flask(__name__)
 
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-db.init_app(app)
+# db.init_app(app)
 
 @app.route("/")
 def home():
