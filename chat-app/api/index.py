30bafa39b8bb94674b86430529b7adce7346caff
bhavit-jfb <jfbrecov@gmail.com>
2026-03-02 00:14:39 +0530

    Update index.py

--- chat-app/api/index.py
+++ chat-app/api/index.py
@@ -1,10 +1,16 @@
+import os
 from flask import Flask
+from extensions import db
 
 app = Flask(__name__)
 
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db.init_app(app)
+
 @app.route("/")
 def home():
-    return "Flask on Vercel is working 🚀"
+    return "Flask working on Vercel 🚀"
 
-# VERY IMPORTANT
 app = app
